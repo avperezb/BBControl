8320cb58537fd56179c85991d072d8ee6d0385b1
avperezb <av.perezb@uniandes.edu.co>
2020-04-17 21:28:22 -0500

    connection state

--- bbcontrol/lib/setup/Pages/Home/home.dart
+++ bbcontrol/lib/setup/Pages/Home/home.dart
@@ -8,12 +8,18 @@ import 'package:overlay_support/overlay_support.dart';
 import '../Services/auth.dart';
 
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
 
+class HomeState extends State<Home>{
+
+  CheckConnectivityState checkConnection = CheckConnectivityState();
+  bool cStatus = true;
   AuthService _auth = AuthService();
   final iconSize = 60.0;
 
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
@@ -30,166 +36,238 @@ class Home extends StatelessWidget {
             )
           ],
         ),
-        body: ListView(
-          children:<Widget>[ Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                height: (MediaQuery.of(context).size.height - AppBar().preferredSize.height - 24.0)/6,
-                child: FlatButton(
-                  padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
-                  color: const Color(0xFF69B3E7),
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => ReservationsList()),);
-                  },
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(Icons.group,
-                          size:iconSize,
-                          color: Colors.white),
-                      Text('Reservations',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ) ,)
-                    ],
-                  ),
+        body: Builder(
+          builder: (context) =>
+              ListView(
+                children: <Widget>[ Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Container(
+                      height: (MediaQuery
+                          .of(context)
+                          .size
+                          .height - AppBar().preferredSize.height - 24.0) / 6,
+                      child: FlatButton(
+                        padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                        color: const Color(0xFF69B3E7),
+                        onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (
+                              context) => ReservationsList()),);
+                        },
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(Icons.group,
+                                size: iconSize,
+                                color: Colors.white),
+                            Text('Reservations',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),)
+                          ],
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            Container(
+                              width: MediaQuery
+                                  .of(context)
+                                  .size
+                                  .width / 2,
+                              height: (MediaQuery
+                                  .of(context)
+                                  .size
+                                  .height - AppBar().preferredSize.height -
+                                  24.0) * 4 / 9,
+                              child: FlatButton(
+                                padding: EdgeInsets.fromLTRB(
+                                    0.0, 90.0, 0.0, 90.0),
+                                color: const Color(0xFFD7384A),
+                                onPressed: () {
+                                  Navigator.push(context, MaterialPageRoute(
+                                      builder: (context) => DrinksTabs()),);
+                                },
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: <Widget>[
+                                    Icon(Icons.local_bar,
+                                        size: iconSize,
+                                        color: Colors.white),
+                                    Text('Drinks',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),)
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Container(
+                              width: MediaQuery
+                                  .of(context)
+                                  .size
+                                  .width / 2,
+                              height: (MediaQuery
+                                  .of(context)
+                                  .size
+                                  .height - AppBar().preferredSize.height -
+                                  24.0) * 2 / 9,
+                              child: FlatButton(
+                                padding: EdgeInsets.fromLTRB(
+                                    0.0, 25.0, 0.0, 25.0),
+                                color: const Color(0xFFFF6B00),
+                                onPressed: () {},
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: <Widget>[
+                                    Icon(Icons.directions_car,
+                                        size: iconSize,
+                                        color: Colors.white),
+                                    Text('Request cab',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),)
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Container(
+                              width: MediaQuery
+                                  .of(context)
+                                  .size
+                                  .width / 2,
+                              height: (MediaQuery
+                                  .of(context)
+                                  .size
+                                  .height - AppBar().preferredSize.height -
+                                  24.0) * 2 / 9,
+                              child: FlatButton(
+                                padding: EdgeInsets.fromLTRB(
+                                    0.0, 25.0, 0.0, 25.0),
+                                color: const Color(0xFFD8AE2D),
+                                onPressed: () {},
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: <Widget>[
+                                    Icon(Icons.local_offer,
+                                        size: iconSize,
+                                        color: Colors.white),
+                                    Text('Special offers',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),)
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Container(
+                              width: MediaQuery
+                                  .of(context)
+                                  .size
+                                  .width / 2,
+                              height: (MediaQuery
+                                  .of(context)
+                                  .size
+                                  .height - AppBar().preferredSize.height -
+                                  24.0) * 4 / 9,
+                              child: Builder(
+                                builder: (context) =>
+                                    FlatButton(
+                                      padding: EdgeInsets.fromLTRB(
+                                          0.0, 90.0, 0.0, 90.0),
+                                      color: const Color(0xFF996480),
+                                      onPressed: () {
+                                        Navigator.push(context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    FoodList()));
+                                        showToast(context);
+                                        if(!cStatus){
+                                          showOverlayNotification((context) {
+                                            return Card(
+                                                margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                                              child: SafeArea(
+                                                child: ListTile(
+                                                  title: Text('Connection Error',
+                                                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white)
+                                                  ),
+                                                  subtitle: Text('Orders will be added when connection is back.',
+                                                    style: TextStyle(fontSize: 16, color: Colors.white),
+                                                  ),
+                                                  trailing: IconButton(
+                                                      icon: Icon(Icons.close, color: Colors.white,),
+                                                      onPressed: () {
+                                                        OverlaySupportEntry.of(context).dismiss();
+                                                      }),
+                                                ),
+                                              ),
+                                            color: Colors.blueGrey,);
+                                          }, duration: Duration(milliseconds: 4000));;
+                                        }
+                                      },
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment
+                                            .center,
+                                        children: <Widget>[
+                                          Icon(Icons.room_service,
+                                              size: iconSize,
+                                              color: Colors.white),
+                                          Text('Food',
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                            ),)
+                                        ],
+                                      ),
+                                    ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: (MediaQuery
+                          .of(context)
+                          .size
+                          .height - AppBar().preferredSize.height - 24.0) / 6,
+                      child: FlatButton(
+                        padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                        color: const Color(0xFFB6B036),
+                        onPressed: () {},
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(Icons.shopping_cart,
+                                size: iconSize,
+                                color: Colors.white),
+                            Text('My order',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),)
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              Row(
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Container(
-                        width: MediaQuery.of(context).size.width/2,
-                        height: (MediaQuery.of(context).size.height - AppBar().preferredSize.height - 24.0)*4/9,
-                        child: FlatButton(
-                          padding: EdgeInsets.fromLTRB(0.0, 90.0, 0.0, 90.0),
-                          color: const Color(0xFFD7384A),
-                          onPressed: () {
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => DrinksTabs()),);
-                          },
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              Icon(Icons.local_bar,
-                                  size: iconSize,
-                                  color: Colors.white),
-                              Text('Drinks',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ) ,)
-                            ],
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width/2,
-                        height: (MediaQuery.of(context).size.height - AppBar().preferredSize.height - 24.0)*2/9,
-                        child: FlatButton(
-                          padding: EdgeInsets.fromLTRB(0.0, 25.0, 0.0, 25.0),
-                          color: const Color(0xFFFF6B00),
-                          onPressed: () {},
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              Icon(Icons.directions_car,
-                                  size:iconSize,
-                                  color: Colors.white),
-                              Text('Request cab',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ) ,)
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Container(
-                        width: MediaQuery.of(context).size.width/2,
-                        height: (MediaQuery.of(context).size.height - AppBar().preferredSize.height - 24.0)*2/9,
-                        child: FlatButton(
-                          padding: EdgeInsets.fromLTRB(0.0, 25.0, 0.0, 25.0),
-                          color: const Color(0xFFD8AE2D),
-                          onPressed: () {},
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              Icon(Icons.local_offer,
-                                  size: iconSize,
-                                  color: Colors.white),
-                              Text('Special offers',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ) ,)
-                            ],
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width/2,
-                        height: (MediaQuery.of(context).size.height - AppBar().preferredSize.height - 24.0)*4/9,
-                        child: FlatButton(
-                          padding: EdgeInsets.fromLTRB(0.0, 90.0, 0.0, 90.0),
-                          color: const Color(0xFF996480),
-                          onPressed: () {
-
-                            someMethod();
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => FoodList()),);
-                            showSimpleNotification(
-                                Text('?jjj'),
-                                background: Colors.green);
-                          },
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              Icon(Icons.room_service,
-                                  size: iconSize,
-                                  color: Colors.white),
-                              Text('Food',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ) ,)
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
                 ],
               ),
-              Container(
-                height: (MediaQuery.of(context).size.height - AppBar().preferredSize.height - 24.0)/6,
-                child: FlatButton(
-                  padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
-                  color: const Color(0xFFB6B036),
-                  onPressed: () {},
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(Icons.shopping_cart,
-                          size: iconSize,
-                          color: Colors.white),
-                      Text('My order',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ) ,)
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-          ],
-        ),
-      );
+        )
+    );
   }
 
-  Future<String> someMethod ()  async{
-      String mensaje = await CheckConnectivity().checkInternetConnectivity();
-      return mensaje;
+  void showToast(BuildContext context) async {
+    await checkConnection.initConnectivity();
+    setState(() {
+      cStatus = checkConnection.getConnectionStatus(context);
+      print(cStatus.toString()+'hhhhhhhhhhhhh');
+    });
   }
 }
+
--- bbcontrol/lib/setup/Pages/Services/connectivity.dart
+++ bbcontrol/lib/setup/Pages/Services/connectivity.dart
@@ -1,17 +1,98 @@
+import 'dart:async';
 import 'package:connectivity/connectivity.dart';
-import 'package:overlay_support/overlay_support.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class CheckConnectivity {
+class CheckConnectivityState {
 
-  Future<String> checkInternetConnectivity() async {
-    var result = await Connectivity().checkConnectivity();
-    if (result == ConnectivityResult.none) {
-      return await 'No internet connection';
-    } else if (result == ConnectivityResult.mobile) {
-      return await 'Connection on mobile data';
-    } else if (result == ConnectivityResult.wifi) {
-      return await 'Connection by wifi';
-    }
+  bool cStatus = true;
+  String _connectionStatus = 'Unknown';
+  final Connectivity _connectivity = Connectivity();
+  //StreamSubscription<ConnectivityResult> _connectivitySubscription;
+
+  void initState() {
+    initConnectivity();
+    //_connectivitySubscription =
+    //_connectivity.onConnectivityChanged.listen(updateConnectionStatus);
   }
 
+/*  void dispose() {
+    _connectivitySubscription.cancel();
+  }*/
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initConnectivity() async {
+    ConnectivityResult result;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      result = await _connectivity.checkConnectivity();
+      print(result);
+      await updateConnectionStatus(result);
+    } catch (e) {
+      print(e.toString());
+    }
+
+    return updateConnectionStatus(result);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    initState();
+  }
+
+  bool getConnectionStatus(BuildContext context){
+    build(context);
+    return cStatus;
+  }
+
+  void setConnectionStatus(bool status){
+    cStatus = status;
+  }
+
+  Future<void> updateConnectionStatus(ConnectivityResult result) async {
+    switch (result) {
+      case ConnectivityResult.wifi:
+        String wifiName, wifiBSSID, wifiIP;
+        cStatus = true;
+        try {
+          wifiName = await _connectivity.getWifiName();
+        } catch (e) {
+          print(e.toString());
+        }
+
+        try {
+          wifiBSSID = await _connectivity.getWifiBSSID();
+        } catch (e) {
+          print(e.toString());
+          wifiBSSID = "Failed to get Wifi BSSID";
+        }
+
+        try {
+          wifiIP = await _connectivity.getWifiIP();
+        } catch (e) {
+          print(e.toString());
+          wifiIP = "Failed to get Wifi IP";
+        }
+
+        _connectionStatus = await '$result\n'
+            'Wifi Name: $wifiName\n'
+            'Wifi BSSID: $wifiBSSID\n'
+            'Wifi IP: $wifiIP\n';
+
+        break;
+      case ConnectivityResult.mobile:
+        _connectionStatus = result.toString();
+        cStatus = true;
+        break;
+      case ConnectivityResult.none:
+        _connectionStatus = result.toString();
+        cStatus = false;
+        break;
+      default:
+        _connectionStatus = 'Failed to get connectivity.';
+        cStatus = true;
+        break;
+    }
+    setConnectionStatus(cStatus);
+  }
 }
